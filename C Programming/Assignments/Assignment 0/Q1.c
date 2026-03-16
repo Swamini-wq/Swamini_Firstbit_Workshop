70f60824d0adbd101bbb01f9070c9fbb02414e4c
Swamini1403 <swaminiwarale@gmail.com>
2026-03-16 18:22:36 +0530

    auto update

--- C Programming/Assignments/Assignment 0/Q1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a=10,b=20 ;
-    int add= a+b;
-    printf("\n addition of two integers ,%d",add);
-
-}
--- C Programming/Assignments/Assignment 0/Q3.c
+++
@@ -1,9 +0,0 @@
-//F=(c*9/5)+32
-#include<stdio.h>
-void main()
-{
-    float cel=14.3,far;
-    far=(cel*9/5)+32;
-    printf("%f is farenhein of %f celciaus",far,cel);
-}
-
